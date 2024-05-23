1b50e364f25e43873a39606e4e34056eec3429bb
Richard <smithr.programming@gmail.com>
2024-05-22 21:12:56 -0700

    started dev on list_shipments + filter

--- src/unofficial_shipengine/core/common/enums.py
+++ src/unofficial_shipengine/core/common/enums.py
@@ -5,3 +5,13 @@ class ValidateAddress(Enum):
     NO_VALIDATION: str = "no_validation"
     VALIDATE_ONLY: str = "validate_only"
     VALIDATE_AND_CLEAN: str = "validate_and_clean"
+
+
+class SortDir(Enum):
+    DESC: str = "desc"
+    ASC: str = "asc"
+
+
+class SortBy(Enum):
+    MODIFIED_AT: str = "modified_at"
+    CREATED_AT: str = "created_at"
